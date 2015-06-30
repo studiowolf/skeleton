3aa71e279921dd51bee249c76be918f859608332
Tom <tom@studiowolf.nl>
2015-06-30 11:11:22 +0200

    updated and tested latest version of autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -26,13 +26,15 @@ if(gutil.env.theme) {
   theme = gutil.env.theme;
 }
 
-// @TODO: Enable prefixer
 gulp.task('styles', function () {
   gulp.src('./content/themes/' + theme + '/assets/stylesheets/styles.scss')
     //.pipe(sass({sourceComments: 'map', sourceMap: 'sass'}))
     .pipe(sass())
     .on('error', handleErrors)
-    //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     // IE7/8 CSS support is disbabled on default!
     .pipe(gulpif(production, minifycss({'noAdvanced': true})))
     .pipe(gulp.dest('content/themes/' + theme + '/compiled-assets/stylesheets'))
